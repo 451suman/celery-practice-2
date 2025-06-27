61155df89fad97107f77b2319a21b26533f854a3
451suman <sumanmushyakhwo@gmail.com>
2025-06-27 07:39:31 +0545

    finish corntab

--- core/celery.py
+++ core/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -13,8 +14,14 @@ app = Celery("core")
 #   should have a `CELERY_` prefix.
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+app.conf.beat_schedule = {
+    "send-mail-every-day-at-6am": {
+        "task": "send_mail_app.tasks.send_mail_func",
+        "schedule": crontab(hour=6, minute=22),
+    }
+}
 
-app.conf.beat_schedule = {}
+
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
